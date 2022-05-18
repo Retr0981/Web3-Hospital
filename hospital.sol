0ba9380c0057933e6d28e9d40d1b0f274ae3ec6b
David Nii Armah <davidarmah991@gmail.com>
2022-05-18 02:57:12 +0000

    Add files via upload

---
+++ hospital.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.7;
+
+contrcat hospitalContract {
+
+    mapping(uint => hospital) public HOSPITAL;
+
+    struct hospital {
+    uint hospital_id;
+    string hospital_name;
+    }
+
+    function add_hospital(uint _id, string memory _name) public {
+     HOSPITAL[_id] = hospital(_id,_name);
+    }
+
+    function get_hospital_data(uint _id) public view returns (hospital memory) {
+     return HOSPITAL[_id];
+    }
+}
